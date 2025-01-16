2aaa52ede8debe521d4590f053c71c314069c6d1
TudorOrban <tudororban2@gmail.com>
2025-01-16 14:12:02 +0200

    Initialize Spring Boot backend

---
+++ backend/src/main/java/com/hilbert/HilbertApplication.java
@@ -0,0 +1,13 @@
+package com.hilbert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HilbertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HilbertApplication.class, args);
+	}
+
+}
