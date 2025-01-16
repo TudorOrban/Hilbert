f4b54c634540cfaff506239329a4c8436a9322bd
TudorOrban <tudororban2@gmail.com>
2025-01-16 19:13:09 +0200

    Start fleshing out User Service

---
+++ backend/src/main/java/com/hilbert/core/user/dto/UserMapper.java
@@ -0,0 +1,15 @@
+package com.hilbert.core.user.dto;
+
+import com.hilbert.core.user.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    @Mapping(source = "user.username", target = "username")
+    @Mapping(source = "user.email", target = "email")
+    UserDataDto userToUserDataDto(User user);
+}
--- backend/src/main/java/com/hilbert/core/user/service/UserServiceImpl.java
+++ backend/src/main/java/com/hilbert/core/user/service/UserServiceImpl.java
@@ -1,9 +1,36 @@
 package com.hilbert.core.user.service;
 
+import com.hilbert.core.user.dto.UserDataDto;
+import com.hilbert.core.user.dto.UserMapper;
+import com.hilbert.core.user.model.User;
+import com.hilbert.core.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
 
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserDataDto getByUsername(String username) {
+        Optional<User> userOpt = this.userRepository.findByUsername(username);
+        if (userOpt.isEmpty()) {
+            // TODO: Throw exception
+        }
+        User foundUser = userOpt.get();
+
+        return this.mapUserToUserDataDto(foundUser);
+    }
+
+    
+    private UserDataDto mapUserToUserDataDto(User user) {
+        return UserMapper.INSTANCE.userToUserDataDto(user);
+    }
 }
