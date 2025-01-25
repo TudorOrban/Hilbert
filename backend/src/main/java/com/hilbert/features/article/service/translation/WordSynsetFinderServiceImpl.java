824b2ec98c4d0c5125212eb442d457b1b88bd779
TudorOrban <tudororban2@gmail.com>
2025-01-25 17:39:33 +0200

    feat(translation): Implement the (word -> Synset ILI) finder algorithm

--- backend/src/main/java/com/hilbert/features/article/service/translation/WordSynsetFinderServiceImpl.java
+++ backend/src/main/java/com/hilbert/features/article/service/translation/WordSynsetFinderServiceImpl.java
@@ -7,101 +7,108 @@ import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.events.Attribute;
 import javax.xml.stream.events.EndElement;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class WordSynsetFinderServiceImpl implements WordSynsetFinderService {
 
     public HashMap<String, List<String>> identifySynsetILIs(List<String> contentWords, Language language) {
-        HashMap<String, List<String>> wordSynsetIds = new HashMap<>();
-
         String filePath = OMWFileFinderUtil.getFilePathByLanguage(language);
 
-        XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
-
         try {
+            XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
             XMLEventReader reader = xmlInputFactory.createXMLEventReader(new FileInputStream(filePath));
 
-            String currentLexicalEntryId = null;
-            String currentMatchedWord = null;
+            return traverseXMLFile(contentWords, reader);
+        } catch (XMLStreamException | FileNotFoundException e) {
+            throw new RuntimeException("Error processing XML file: ", e);
+        }
+    }
 
-            while (reader.hasNext()) {
-                XMLEvent nextEvent = reader.nextEvent();
+    private HashMap<String, List<String>> traverseXMLFile(List<String> contentWords, XMLEventReader reader) throws XMLStreamException {
+        HashMap<String, List<String>> wordSynsetIds = new HashMap<>();
+        HashMap<String, List<String>> wordSynsetILIs = new HashMap<>();
+        String currentLexicalEntryId = null;
+        String currentMatchedWord = null;
 
-                if (nextEvent.isStartElement()) {
-                    StartElement startElement = nextEvent.asStartElement();
+        while (reader.hasNext()) {
+            XMLEvent nextEvent = reader.nextEvent();
 
-                    switch (startElement.getName().getLocalPart()) {
-                        case "LexicalEntry":
-                            Attribute id = startElement.getAttributeByName(new QName("id"));
-                            currentLexicalEntryId = id.getValue();
-                            break;
-                        case "Lemma":
-                            Attribute writtenForm = startElement.getAttributeByName(new QName("writtenForm"));
-                            String word = writtenForm.getValue();
-
-                            if (contentWords.contains(word)) {
-                                currentMatchedWord = word;
-                            } else {
-                                currentMatchedWord = null;
-                            }
-                            break;
-                        case "Sense":
-                            if (currentLexicalEntryId == null || currentMatchedWord == null) {
-                                break;
-                            }
-
-                            Attribute synset = startElement.getAttributeByName(new QName("synset"));
-                            String synsetId = synset.getValue();
-
-                            wordSynsetIds.computeIfAbsent(currentMatchedWord, k -> new ArrayList<>()).add(synsetId);
-                            break;
-                    }
-                }
-                if (nextEvent.isEndElement()) {
-                    EndElement endElement = nextEvent.asEndElement();
-                    if (endElement.getName().getLocalPart().equals("LexicalEntry")) {
-                        currentLexicalEntryId = null;
-                        currentMatchedWord = null;
-                    }
+            if (nextEvent.isEndElement()) {
+                EndElement endElement = nextEvent.asEndElement();
+                if (endElement.getName().getLocalPart().equals("LexicalEntry")) {
+                    currentLexicalEntryId = null;
+                    currentMatchedWord = null;
                 }
             }
-        } catch (XMLStreamException | FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
 
-        HashMap<String, List<String>> wordSynsetILIs = new HashMap<>();
+            if (!nextEvent.isStartElement()) {
+                continue;
+            }
+            StartElement startElement = nextEvent.asStartElement();
+
+            switch (startElement.getName().getLocalPart()) {
+                case "LexicalEntry":
+                    currentLexicalEntryId = startElement.getAttributeByName(new QName("id")).getValue();
+                    break;
+                case "Lemma":
+                    String word = startElement.getAttributeByName(new QName("writtenForm")).getValue();
+
+                    if (contentWords.contains(word)) {
+                        currentMatchedWord = word;
+                    } else {
+                        currentMatchedWord = null;
+                    }
+                    break;
+                case "Sense":
+                    if (currentLexicalEntryId == null || currentMatchedWord == null) {
+                        break;
+                    }
+
+                    String synsetId = startElement.getAttributeByName(new QName("synset")).getValue();
+
+                    wordSynsetIds.computeIfAbsent(currentMatchedWord, k -> new ArrayList<>()).add(synsetId);
+                    break;
+                // Synset case can be handled in the same traversal as Synset elements are always after all the LexicalEntries
+                case "Synset":
+                    String elementSynsetId = startElement.getAttributeByName(new QName("id")).getValue();
+                    String synsetILI = startElement.getAttributeByName(new QName("ili")).getValue();
+
+                    for (Map.Entry<String, List<String>> entry : wordSynsetIds.entrySet()) {
+                        if (entry.getValue().contains(elementSynsetId)) {
+                            wordSynsetILIs.computeIfAbsent(entry.getKey(), k -> new ArrayList<>()).add(synsetILI);
+                            break;
+                        }
+                    }
+                    break;
+            }
+        }
 
         return wordSynsetILIs;
     }
-
-    private void handleLexicalEntry(StartElement startElement) {
-
-    }
-
 }
 
 /*
-
-    <LexicalEntry id="omw-fr-6_août-n">
-      <Lemma writtenForm="6 août" partOfSpeech="n" />
-      <Sense id="omw-fr-6_août-15299367-n" synset="omw-fr-15299367-n" />
-      <Sense id="omw-fr-21-15299367-n" synset="omw-fr-12499365-n" />
-    </LexicalEntry>
-    <LexicalEntry id="omw-fr-11_septembre-n">
-      <Lemma writtenForm="11 septembre" partOfSpeech="n" />
-      <Sense id="omw-fr-11_septembre-15300051-n" synset="omw-fr-15300051-n" />
-    </LexicalEntry>
-    <Synset id="omw-fr-00001740-a" ili="i1" partOfSpeech="a" members="omw-fr-comptable-00001740-a" />
-    <Synset id="omw-fr-00001740-n" ili="i35545" partOfSpeech="n" members="omw-fr-entité-00001740-n" />
-
+ *   Example from omw-fr:
+ *
+ *   <LexicalEntry id="omw-fr-6_août-n">
+ *     <Lemma writtenForm="6 août" partOfSpeech="n" />
+ *     <Sense id="omw-fr-6_août-15299367-n" synset="omw-fr-15299367-n" />
+ *     <Sense id="omw-fr-21-15299367-n" synset="omw-fr-12499365-n" />
+ *   </LexicalEntry>
+ *   <LexicalEntry id="omw-fr-11_septembre-n">
+ *     <Lemma writtenForm="11 septembre" partOfSpeech="n" />
+ *     <Sense id="omw-fr-11_septembre-15300051-n" synset="omw-fr-15300051-n" />
+ *   </LexicalEntry>
+ *   <Synset id="omw-fr-00001740-a" ili="i1" partOfSpeech="a" members="omw-fr-comptable-00001740-a" />
+ *   <Synset id="omw-fr-00001740-n" ili="i35545" partOfSpeech="n" members="omw-fr-entité-00001740-n" />
+ *
  */
