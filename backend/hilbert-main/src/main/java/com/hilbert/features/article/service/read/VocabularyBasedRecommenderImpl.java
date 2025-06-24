4354fb3e0dbcdc75197b9904f6bd74d657b0fde9
TudorOrban <tudororban2@gmail.com>
2025-06-24 11:53:08 +0300

    feat(vocab): Work further on vocabulary-based article recommendations

--- backend/hilbert-main/src/main/java/com/hilbert/features/article/service/read/VocabularyBasedRecommenderImpl.java
+++ backend/hilbert-main/src/main/java/com/hilbert/features/article/service/read/VocabularyBasedRecommenderImpl.java
@@ -4,6 +4,7 @@ import com.hilbert.features.article.model.Article;
 import com.hilbert.features.vocabulary.model.VocabularyData;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,10 +17,22 @@ public class VocabularyBasedRecommenderImpl implements VocabularyBasedRecommende
     private List<Article> recommendArticles(VocabularyData data) {
         HashMap<String, List<LocalDateTime>> wordsToBeRefreshed = new HashMap<>();
 
+        HashMap<String, Float> recencyScores = this.determineRecencyScores(data);
+
+
+        return new ArrayList<>();
+    }
+
+    private HashMap<String, Float> determineRecencyScores(VocabularyData data) {
+        HashMap<String, Float> recencyScores = new HashMap<>();
+
+        LocalDate now = LocalDate.now();
+
         int TOO_MANY_THRESHOLD = 50;
         int TOO_SOON_THRESHOLD = 10; // 10 refreshes of a word per day should be enough
 
         for (Map.Entry<String, List<LocalDateTime>> entry : data.getWordsReadDates().entrySet()) {
+            String word = entry.getKey();
             List<LocalDateTime> wordDates = entry.getValue();
             if (wordDates.isEmpty()) {
                 continue;
@@ -30,22 +43,19 @@ public class VocabularyBasedRecommenderImpl implements VocabularyBasedRecommende
                 continue; // No need for refresh
             }
 
-            int dayCounter = 0;
-            LocalDateTime lastDate = wordDates.getFirst();
+            List<LocalDateTime> sortedDates = wordDates.stream().sorted().toList();
+            LocalDate firstDate = sortedDates.getLast().toLocalDate();
+            int totalRecency = firstDate.compareTo(now);
 
-            for (LocalDateTime date : wordDates) {
-                if (lastDate.toLocalDate().equals(date.toLocalDate())) {
-                    dayCounter++;
-                } else {
-                    dayCounter = 0;
-                }
-                lastDate = date;
+            for (LocalDateTime date : sortedDates) {
+                int recency = date.toLocalDate().compareTo(now);
+                float recencyRatio = (float) recency / totalRecency;
 
+                recencyScores.merge(word, recencyRatio, Float::sum);
             }
 
         }
 
-
-        return new ArrayList<>();
+        return recencyScores;
     }
 }
